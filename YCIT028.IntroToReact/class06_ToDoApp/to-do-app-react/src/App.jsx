4fac5e7ff1f876be1d6a68f86bf36ff90e277baa
PJ Angeloni <pj.angeloni@gmail.com>
2023-06-15 17:21:06 -0400

    initial changes for to-do-app-react

--- YCIT028.IntroToReact/class06_ToDoApp/to-do-app-react/src/App.jsx
+++ YCIT028.IntroToReact/class06_ToDoApp/to-do-app-react/src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.scss";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div id="app">
+      <h1>My Todo 🗒️</h1>
+
+      <div className="container">
+        <form>
+          <h2>Add Item</h2>
+          <input
+            type="text"
+            id="title"
+            placeholder="Title"
+            className="form-control"
+            autocomplete="off"
+          />
+          <button>Add</button>
+        </form>
+
+        <div className="items">
+          <h2>My Items</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
