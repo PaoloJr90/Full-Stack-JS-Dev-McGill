be01e0a0306b96d5f2886fe1ae948a0457bd9804
PJ Angeloni <pj.angeloni@gmail.com>
2023-06-15 22:43:39 -0400

    completed assignment04 and new course materials added

--- YCIT028.IntroToReact/class06_ToDoApp/to-do-app-react/src/App.jsx
+++ YCIT028.IntroToReact/class06_ToDoApp/to-do-app-react/src/App.jsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("todo-items")) || []
+  );
+
+  // useEffect(() => {
+  //   console.log("useEffect is called");
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todo-items", JSON.stringify(items));
+  }, [items]);
+
   return (
     <div id="app">
       <h1>My Todo 🗒️</h1>
@@ -13,13 +27,63 @@ function App() {
             id="title"
             placeholder="Title"
             className="form-control"
-            autocomplete="off"
+            autoComplete="off"
+            value={input}
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
           />
-          <button>Add</button>
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              if (input === "") {
+                alert("input value cannot be empty");
+                return;
+              }
+              const newItems = [...items];
+              newItems.push(input);
+              setItems(newItems);
+              setInput("");
+            }}
+          >
+            Add
+          </button>
+          <button
+            className="red"
+            onClick={(event) => {
+              event.preventDefault();
+              setItems([]);
+            }}
+          >
+            Reset
+          </button>
         </form>
 
         <div className="items">
           <h2>My Items</h2>
+          <ol>
+            {items.map((item, index) => {
+              return (
+                <li key={`item-${index}`}>
+                  <h3>{item}</h3>
+                  <button
+                    className="delete"
+                    onClick={() => {
+                      // console.log(items, index);
+                      const newItems = items.filter((item, indexItem) => {
+                        return index !== indexItem;
+                      });
+                      // const newItems = [
+                      //   ...items.slice(0, index),
+                      //   ...items.slice(index + 1, items.length),
+                      // ];
+                      setItems(newItems);
+                    }}
+                  ></button>
+                </li>
+              );
+            })}
+          </ol>
         </div>
       </div>
     </div>
